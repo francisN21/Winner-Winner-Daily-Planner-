1cf33e0a4d5e6471aeb074a04e191e77e7400d53
Francisco Rones <ininorones@gmail.com>
2020-10-31 19:08:14 -0700

    New: Onclick function to save text to local storage added and working.

--- Develop/app.js
+++ Develop/app.js
@@ -6,6 +6,17 @@ const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 // document.querySelector("#currentDay").textContent = currentTime;
 $("#currentDay").text(currentTime);
 
+// saveBTN user input eventlistener
+$(".saveBtn").on("click", function(e){
+    e.preventDefault();
+    // get the values from the description class and id
+
+    let text = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+    // saves time and text on to local storage
+    localStorage.setItem(time, text);
+    console.log(this)
+})
 
 
 
